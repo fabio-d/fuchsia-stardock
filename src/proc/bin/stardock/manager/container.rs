b87f5fc075666c6d40b30f38311fcec8a33ca164
Fabio D'Urso <fbdurso@gmail.com>
2021-11-28 01:27:58 +0100

    [stardock] Preliminary start command that always runs hello_starnix

--- src/proc/bin/stardock/manager/container.rs
+++ src/proc/bin/stardock/manager/container.rs
@@ -2,6 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use anyhow::Error;
+use fidl::endpoints::{create_proxy, Proxy};
+use fidl::HandleBased;
+use fidl_fuchsia_component as fcomponent;
+use fidl_fuchsia_component_decl as fdecl;
+use fidl_fuchsia_process as fprocess;
+use fidl_fuchsia_starnix_developer as fstardev;
+use fuchsia_component::client::connect_to_protocol;
+use fuchsia_runtime::{HandleInfo, HandleType};
 use log::info;
 use rand::Rng;
 use stardock_common::digest;
@@ -9,6 +18,8 @@ use std::collections::HashMap;
 use std::rc::Rc;
 
 use crate::image;
+
+static CONTAINER_COLLECTION_NAME: &str = "container";
 
 #[derive(Debug)]
 pub struct Container {
@@ -28,6 +39,45 @@ impl Container {
 
     pub fn image_id(&self) -> &digest::Sha256Digest {
         self.image.id()
+    }
+
+    pub async fn run(&self) -> Result<(), Error> {
+        // TODO: prevent multiple running instances of the same container
+
+        let realm = connect_to_protocol::<fcomponent::RealmMarker>()
+            .expect("failed to obtain Realm proxy");
+
+        let (shell_controller, server) =
+            create_proxy::<fstardev::ShellControllerMarker>().unwrap();
+
+        let args = fcomponent::CreateChildArgs {
+            numbered_handles: Some(vec![
+                fprocess::HandleInfo {
+                    handle: server.into_channel().into_handle(),
+                    id: HandleInfo::new(HandleType::User0, 0).as_raw(),
+                },
+            ]),
+            ..fcomponent::CreateChildArgs::EMPTY
+        };
+
+        let child_decl = fdecl::Child {
+            name: Some(self.id.as_str().to_string()),
+            url: Some("fuchsia-pkg://fuchsia.com/hello-starnix#meta/hello_starnix.cm".to_string()),
+            startup: Some(fdecl::StartupMode::Lazy),
+            ..fdecl::Child::EMPTY
+        };
+
+        info!("Container {}: starting", self.id.as_str());
+        let mut collection_ref =
+            fdecl::CollectionRef { name: CONTAINER_COLLECTION_NAME.to_string() };
+        realm.create_child(&mut collection_ref, child_decl, args).await?
+            .map_err(|e| anyhow::anyhow!("failed to create child: {:?}", e))?;
+
+        // Wait for starnix task to exit
+        shell_controller.on_closed().await?;
+        info!("Container {}: exited", self.id.as_str());
+
+        Ok(())
     }
 }
 
