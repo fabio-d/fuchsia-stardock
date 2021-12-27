587c663ef98eb55da5eefeabe0d8f729ae4786a0
Fabio D'Urso <fbdurso@gmail.com>
2021-12-28 00:58:31 +0100

    [starnix] Implement mkdir and rmdir syscalls
    Test: gvisor-glibc/mkdir_test --gtest_filter=*.CanCreateWritableDir:*.HonorsUmask:*.HonorsUmask2
          gvisor-glibc/unlink_test --gtest_filter=*.DirNotEmpty:*.Rmdir:RmdirTest.CannotRemoveDots

--- src/proc/bin/starnix/fs/syscalls.rs
+++ src/proc/bin/starnix/fs/syscalls.rs
@@ -495,6 +495,14 @@ pub fn sys_mkdirat(
     Ok(SUCCESS)
 }
 
+pub fn sys_mkdir(
+    current_task: &CurrentTask,
+    user_path: UserCString,
+    mode: FileMode,
+) -> Result<SyscallResult, Errno> {
+    sys_mkdirat(current_task, FdNumber::AT_FDCWD, user_path, mode)
+}
+
 pub fn sys_mknodat(
     current_task: &CurrentTask,
     dir_fd: FdNumber,
@@ -576,6 +584,13 @@ pub fn sys_unlink(
     user_path: UserCString,
 ) -> Result<SyscallResult, Errno> {
     sys_unlinkat(current_task, FdNumber::AT_FDCWD, user_path, 0)
+}
+
+pub fn sys_rmdir(
+    current_task: &CurrentTask,
+    user_path: UserCString,
+) -> Result<SyscallResult, Errno> {
+    sys_unlinkat(current_task, FdNumber::AT_FDCWD, user_path, AT_REMOVEDIR)
 }
 
 pub fn sys_renameat(
