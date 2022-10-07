3e89fc95bb75b183644d73a83d05d30dcdaa4738
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 14:55:29 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/sdk/merger/merge.py
+++ scripts/sdk/merger/merge.py
@@ -62,7 +62,26 @@ def _open_archive(archive, directory):
         # This is vastly more efficient than accessing files one by one via
         # the tarfile API.
         with tarfile.open(archive) as archive_file:
-            archive_file.extractall(temp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive_file, temp_dir)
         try:
             yield temp_dir
         finally:
